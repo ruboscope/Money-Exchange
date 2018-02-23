ed4afc988791562b02d691833109958515ed7c0e
Zhukov Andrey <AS.Zhukow@BelVeb.by>
2018-02-23 17:32:20 +0300

    - подправлена функция, отрабатывает 60% тестов, не работает на значения меньше 50(11строка)

--- src/index.js
+++ src/index.js
@@ -3,18 +3,17 @@ module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
     minimumNumber = {};
-    if (currency > 9999) {
-        minimumNumber[error] = "You are rich, my friend! We don't have so much coins for exchange";
-    }
-    else {
-        find(currency);
-    }
-    return minimumNumber;
+    function find(descr, val) {
+        if (currency > 9999) {
+            minimumNumber['error'] = "You are rich, my friend! We don't have so much coins for exchange";
+            return minimumNumber;
+        }
+        else if (currency >= val || Object.values(minimumNumber) != undefined) {
+            minimumNumber[descr] = Math.floor(currency / val);
+            currency = currency % val;
+            return find('Q', 25) || find('D', 10) || find('N', 5) || find('P', 1);
+        }
+        else if(currency == 0){ return minimumNumber; }
+    };
+    return find('H', 50);
 }
-function find(descr, val) {
-    if (currency >= val) {
-        minimumNumber[descr] = currency / val;
-        currency = currency % val;
-        return find('H', 50) || find('Q', 25) || find('D', 10) || find('N', 5) || find('P', 1);
-    }
-};
