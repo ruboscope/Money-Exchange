b096d2731d67ab1fb0082ff2987a348b51c96933
Andrey <ruboscope@gmail.com>
2018-02-23 23:37:22 +0300

    - исправлена и доработана реализация алгоритма

--- src/index.js
+++ src/index.js
@@ -5,15 +5,33 @@ module.exports = function makeExchange(currency) {
     minimumNumber = {};
     function find(descr, val) {
         if (currency > 9999) {
+            // при превышении лимита выводим сообщение
             minimumNumber['error'] = "You are rich, my friend! We don't have so much coins for exchange";
             return minimumNumber;
         }
-        else if (currency >= val || Object.values(minimumNumber) != undefined) {
+        else if (currency <= 0) {
+            // при отрицательных значениях, при нулевых, или при разборе валюты, когда в остатке получаем 0, то выводим результат.
+            for (var key in minimumNumber) {
+                if (minimumNumber[key] == 0) {
+                    delete minimumNumber[key];// чистим объект от наличия нулевых валют
+                }
+            }
+            return minimumNumber;
+        }
+        else if (currency >= val || isEmpty(minimumNumber)) {
             minimumNumber[descr] = Math.floor(currency / val);
             currency = currency % val;
             return find('Q', 25) || find('D', 10) || find('N', 5) || find('P', 1);
         }
-        else if(currency == 0){ return minimumNumber; }
     };
     return find('H', 50);
+}
+//функция проверки объекта на пустоту
+function isEmpty(myObject) {
+    for (var key in myObject) {
+        if (myObject.hasOwnProperty(key)) {
+            return false;
+        }
+    }
+    return true;
 }
