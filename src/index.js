4799598f7edffbf32445728d64f93030773f71e2
Andrei Zhukau <ruboscope@gmail.com>
2018-10-17 22:33:46 +0300

    feat: more efficient algorithm with es6

--- src/index.js
+++ src/index.js
@@ -1,37 +1,32 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-    minimumNumber = {};
-    function find(descr, val) {
-        if (currency > 9999) {
-            // при превышении лимита выводим сообщение
-            minimumNumber['error'] = "You are rich, my friend! We don't have so much coins for exchange";
-            return minimumNumber;
-        }
-        else if (currency <= 0) {
-            // при отрицательных значениях, при нулевых, или при разборе валюты, когда в остатке получаем 0, то выводим результат.
-            for (var key in minimumNumber) {
-                if (minimumNumber[key] == 0) {
-                    delete minimumNumber[key];// чистим объект от наличия нулевых валют
-                }
-            }
-            return minimumNumber;
-        }
-        else if (currency >= val || isEmpty(minimumNumber)) {
-            minimumNumber[descr] = Math.floor(currency / val);
-            currency = currency % val;
-            return find('Q', 25) || find('D', 10) || find('N', 5) || find('P', 1);
-        }
-    };
-    return find('H', 50);
-}
-//функция проверки объекта на пустоту
-function isEmpty(myObject) {
-    for (var key in myObject) {
-        if (myObject.hasOwnProperty(key)) {
-            return false;
-        }
+  // Your code goes here!
+  // Return an object containing the minimum number of coins needed to make change
+
+  const currencies = {
+    H: 50,
+    Q: 25,
+    D: 10,
+    N: 5,
+    P: 1
+  };
+
+
+  if (currency > 10000) {
+    return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+  }
+  
+
+  let currentCurrency = currency;
+  
+  const result = {};
+
+  for (let key in currencies) {
+    if (currentCurrency >= currencies[key]) {
+      const value = Math.floor(currentCurrency / currencies[key]);
+      result[key] = value;
+      currentCurrency -= value * currencies[key];
     }
-    return true;
+  }
+
+  return result;
 }
